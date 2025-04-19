cae3e74ddabc8e65731eca5209144de1df7d3415
Frédéric Dewinne <fred@fd-management.net>
2025-04-19 17:41:01 +0200

    fix: Dispatcher kms permission role mismatch (#4547)

--- modules/webhook/eventbridge/dispatcher.tf
+++ modules/webhook/eventbridge/dispatcher.tf
@@ -117,7 +117,7 @@ resource "aws_iam_role_policy" "dispatcher_sqs" {
 
 resource "aws_iam_role_policy" "dispatcher_kms" {
   name = "kms-policy"
-  role = aws_iam_role.webhook_lambda.name
+  role = aws_iam_role.dispatcher_lambda.name
 
   policy = templatefile("${path.module}/../policies/lambda-kms.json", {
     kms_key_arn = var.config.kms_key_arn != null ? var.config.kms_key_arn : "arn:${var.config.aws_partition}:kms:::CMK_NOT_IN_USE"
