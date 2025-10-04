wadherv/terraform-aws-github-runner

commit 20eeead48a4547e1f29257bbe771c14917d5db96

feat: add cpu_options for EC2 launch template (#4789)
This PR adds a feature which enables to use of `cpu_options` in the
runner config.

Context: We have a workload where hyperthreading hurts performance. With
this option, we can opt-out of hyperthreading for a certain runner
configuration.

Tested by applying to our internal dev infra.

Co-authored-by: Niek Palm <npalm@users.noreply.github.com>

modules/runners/main.tf
@@ -160,6 +160,11 @@ resource "aws_launch_template" "runner" {
     }
   }
 
+  cpu_options {
+    core_count       = var.cpu_options != null ? var.cpu_options.core_count : null
+    threads_per_core = var.cpu_options != null ? var.cpu_options.threads_per_core : null
+  }
+
   monitoring {
     enabled = var.enable_runner_detailed_monitoring
   }

main.tf
@@ -208,6 +208,7 @@ module "runners" {
   runner_additional_security_group_ids = var.runner_additional_security_group_ids
   metadata_options                     = var.runner_metadata_options
   credit_specification                 = var.runner_credit_specification
+  cpu_options                          = var.runner_cpu_options
 
   enable_runner_binaries_syncer    = var.enable_runner_binaries_syncer
   lambda_s3_bucket                 = var.lambda_s3_bucket

modules/multi-runner/runners.tf
@@ -56,6 +56,7 @@ module "runners" {
   runner_additional_security_group_ids = try(coalescelist(each.value.runner_config.runner_additional_security_group_ids, var.runner_additional_security_group_ids), [])
   metadata_options                     = each.value.runner_config.runner_metadata_options
   credit_specification                 = each.value.runner_config.credit_specification
+  cpu_options                          = each.value.runner_config.cpu_options
 
   enable_runner_binaries_syncer    = each.value.runner_config.enable_runner_binaries_syncer
   lambda_s3_bucket                 = var.lambda_s3_bucket

modules/multi-runner/variables.tf
@@ -126,6 +126,10 @@ variable "multi_runner_config" {
         idleCount        = number
         evictionStrategy = optional(string, "oldest_first")
       })), [])
+      cpu_options = optional(object({
+        core_count       = number
+        threads_per_core = number
+      }), null)
       runner_log_files = optional(list(object({
         log_group_name   = string
         prefix_log_group = bool

modules/runners/variables.tf
@@ -662,6 +662,15 @@ variable "credit_specification" {
   }
 }
 
+variable "cpu_options" {
+  description = "The CPU options for the instance. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template#cpu-options for details. Note that not all instance types support CPU options, see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-optimize-cpu.html#instance-cpu-options"
+  type = object({
+    core_count       = number
+    threads_per_core = number
+  })
+  default = null
+}
+
 variable "enable_jit_config" {
   description = "Overwrite the default behavior for JIT configuration. By default JIT configuration is enabled for ephemeral runners and disabled for non-ephemeral runners. In case of GHES check first if the JIT config API is avaialbe. In case you upgradeing from 3.x to 4.x you can set `enable_jit_config` to `false` to avoid a breaking change when having your own AMI."
   type        = bool

variables.tf
@@ -873,6 +873,15 @@ variable "runner_credit_specification" {
   }
 }
 
+variable "runner_cpu_options" {
+  description = "TThe CPU options for the instance. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template#cpu-options for details. Note that not all instance types support CPU options, see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-optimize-cpu.html#instance-cpu-options"
+  type = object({
+    core_count       = number
+    threads_per_core = number
+  })
+  default = null
+}
+
 variable "enable_jit_config" {
   description = "Overwrite the default behavior for JIT configuration. By default JIT configuration is enabled for ephemeral runners and disabled for non-ephemeral runners. In case of GHES check first if the JIT config API is avaialbe. In case you upgradeing from 3.x to 4.x you can set `enable_jit_config` to `false` to avoid a breaking change when having your own AMI."
   type        = bool

commit 8a1cd6f5c7a956d15d7eafa9f45ea5405179887a

fix: prevent unnecessary updates when cpu_options is unset (#4806)
This PR will replace:

```hcl
cpu_options {
  core_count       = var.cpu_options != null ? var.cpu_options.core_count : null
  threads_per_core = var.cpu_options != null ? var.cpu_options.threads_per_core : null
}
```

with:

```hcl
dynamic "cpu_options" {
  for_each = var.cpu_options != null ? [var.cpu_options] : []
  content {
    core_count       = try(cpu_options.value.core_count, null)
    threads_per_core = try(cpu_options.value.threads_per_core, null)
  }
}
```

This way, if `cpu_options` is not set, Terraform will **not force a
resource update every time**.

This issue was introduced in PR #4789 


<img width="346" height="94" alt="image"
src="https://github.com/user-attachments/assets/5a3ca60d-7300-4957-91c3-ff87c2cd5b62"
/>

modules/runners/main.tf
@@ -160,9 +160,12 @@ resource "aws_launch_template" "runner" {
     }
   }
 
-  cpu_options {
-    core_count       = var.cpu_options != null ? var.cpu_options.core_count : null
-    threads_per_core = var.cpu_options != null ? var.cpu_options.threads_per_core : null
+  dynamic "cpu_options" {
+    for_each = var.cpu_options != null ? [var.cpu_options] : []
+    content {
+      core_count       = try(cpu_options.value.core_count, null)
+      threads_per_core = try(cpu_options.value.threads_per_core, null)
+    }
   }
 
   monitoring {