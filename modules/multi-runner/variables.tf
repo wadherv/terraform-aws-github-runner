aa7d4f5f2d54af1ea44c2196dbf7794c7e76f413
Vishal Wadhera <30888002+wadherv@users.noreply.github.com>
2026-08-24 10:49:19 +0530

    Update variables.tf

--- modules/multi-runner/variables.tf
+++ modules/multi-runner/variables.tf
@@ -368,6 +368,12 @@ variable "webhook_lambda_timeout" {
   default     = 10
 }
 
+variable "webhook_xray_github_latency_enabled" {
+  description = "Add X-Ray instrumentation (an annotation plus a synthetic 'github' node) measuring the delay between a GitHub workflow_job event's created_at timestamp and this Lambda's invocation. Disabled by default since the synthetic node intentionally backdates its X-Ray start_time, an unusual tracing pattern."
+  type        = bool
+  default     = false
+}
+
 variable "role_permissions_boundary" {
   description = "Permissions boundary that will be added to the created role for the lambda."
   type        = string
